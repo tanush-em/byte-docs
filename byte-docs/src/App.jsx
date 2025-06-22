725acb69cf4d3638f05cd27f1b2132f3a888d560
Tanush T M <tanushtm.work@gmail.com>
2025-06-22 08:36:40 +0530

    tailwind config added

--- byte-docs/src/App.jsx
+++ byte-docs/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router';
 import HomePage from './pages/HomePage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
+import { toast } from 'react-hot-toast';
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <Route path="/create" element={<CreatePage />} />
             <Route path="/notes/:id" element={<NoteDetailPage />} />
           </Routes>
+          <button onClick={() => toast.error("Content copied!")}>Click me</button>
 
         </div>
     )
