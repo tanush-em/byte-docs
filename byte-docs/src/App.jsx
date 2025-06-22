dd40a3063db1d89e4d741f0295e010b66de8a6fd
Tanush T M <tanushtm.work@gmail.com>
2025-06-22 13:49:23 +0530

    daisyui setup complete

--- byte-docs/src/App.jsx
+++ byte-docs/src/App.jsx
@@ -1,20 +1,18 @@
-import {Routes, Route} from 'react-router';
-import HomePage from './pages/HomePage';
-import CreatePage from './pages/CreatePage';
-import NoteDetailPage from './pages/NoteDetailPage';
-import { toast } from 'react-hot-toast';
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import { toast } from "react-hot-toast";
 
 const App = () => {
-    return (
-        <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/create" element={<CreatePage />} />
-            <Route path="/notes/:id" element={<NoteDetailPage />} />
-          </Routes>
-          <button onClick={() => toast.error("Content copied!")}>Click me</button>
-
-        </div>
-    )
-}
+  return (
+    <div data-theme="luxury">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/notes/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
 export default App;
